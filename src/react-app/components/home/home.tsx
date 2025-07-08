f9ef9c4b53edb3f53e19c3dbd85fd1b316e86374
William Jacobsen <villi05.v.j@gmail.com>
2025-07-08 21:38:04 +0200

    hero sub text update

--- src/react-app/components/home/home.tsx
+++ src/react-app/components/home/home.tsx
@@ -7,15 +7,6 @@ function Home() {
       <img src={HomeImg} className="home-img" />
       <h1 className="home-title">Combat Boxing Randers</h1>
       <p className="home-description">
-        Vil du i bedre form, lære selvdisciplin og blive en del af et stærkt
-        fællesskab? <br />
-        Combat Boxing Randers giver dig muligheden for at udvikle dig fysisk og
-        mentalt gennem boksning.
-        <br />
-        <br />
-        Eller
-        <br />
-        <br />
         Drømmer du om at prøve kræfter med bokseringen - eller vil du bare have
         effektiv træning med mening? Vi tilbyder seriøs boksetræning med plads
         til både begyndere og øvede.
